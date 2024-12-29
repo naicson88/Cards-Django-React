521938627cdc6660af04d33458901253365e4200
a91628319 <naicson88@hotmail.com>
2024-12-29 20:28:14 -0300

    first

--- cards_python_react/crawler/models.py
+++ cards_python_react/crawler/models.py
@@ -15,20 +15,5 @@ class Crawler(models.Model):
     def teste(serialized):
         print(serialized)
     
-class DeckCrawler(scrapy.Spider):
-    
-    def __str__(self):
-        return self
-    
-    name = 'collection_deck'
-    allowed_domains = ['yugipedia.com']
-    start_urls = 'https://yugipedia.com/wiki/Battle_Pack:_Epic_Dawn'
-    
-    def parse(self, response):
-        for infos in response.css('.wikitable tr'):
-            yield{
-                'setcode': infos.css('td:nth-child(1) a::text').get(),
-                'quantity': infos.css('td:nth-child(6)::text').get()        
-            }
-            pass
+
         
